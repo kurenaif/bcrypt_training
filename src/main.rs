785d9c5ed82ca48db79102894321cdf9496735ed
kurenaif <antyobido@gmail.com>
2021-04-03 20:20:05 +0900

    update cargo.toml for profile

--- src/main.rs
+++ src/main.rs
@@ -14,5 +14,8 @@ use kurebcrypt::*;
 fn main() {
     let salt: [u8; 16] =  [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
     let password = "password".as_bytes();
-    println!("{}", bcrypt(5, &salt, password));
+    for i in 0..1000  {
+        bcrypt(5, &salt, password);
+        // println!("{}", bcrypt(5, &salt, password));
+    }
 }
